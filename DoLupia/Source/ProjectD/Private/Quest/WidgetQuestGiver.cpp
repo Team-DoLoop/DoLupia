9ba9b09a7ab4a928acbaef2bc8f9c4ac80e2f078
yunju6528@duksung.ac.kr <yunju6528@duksung.ac.kr>
2024-05-07 00:07:45 +0900

    WidgetQuestGiver 추가

---
+++ DoLupia/Source/ProjectD/Private/Quest/WidgetQuestGiver.cpp
@@ -0,0 +1,48 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Quest/WidgetQuestGiver.h"
+#include "Components/Button.h"
+#include "Components/TextBlock.h"
+#include "Characters/ProjectDPlayerController.h"
+
+void UWidgetQuestGiver::NativeConstruct()
+{
+
+    if (btn_Accept)
+    {
+        btn_Accept->OnClicked.AddDynamic( this , &UWidgetQuestGiver::OnAcceptClicked );
+    }
+
+    if (btn_Decline)
+    {
+        btn_Decline->OnClicked.AddDynamic( this , &UWidgetQuestGiver::OnDeclineClicked );
+    }
+
+    APlayerController* PlayerController = GetWorld()->GetFirstPlayerController();
+    auto PlayerControllerD = Cast<AProjectDPlayerController>( PlayerController );
+
+    if (PlayerControllerD) {
+        FInputModeUIOnly InputMode;
+        //if(btn_Accept)
+            //InputMode.SetWidgetToFocus(btn_Accept);
+        PlayerControllerD->SetInputMode( InputMode );
+        PlayerControllerD->bShowMouseCursor = true;
+    }
+    
+}
+
+void UWidgetQuestGiver::OnAcceptClicked()
+{
+
+}
+
+void UWidgetQuestGiver::OnDeclineClicked()
+{
+    RemoveFromParent();
+}
+
+void UWidgetQuestGiver::OnMouseChange()
+{
+
+}
---
+++ DoLupia/Source/ProjectD/Public/Quest/WidgetQuestGiver.h
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "WidgetQuestGiver.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTD_API UWidgetQuestGiver : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+    virtual void NativeConstruct() override;
+
+    UFUNCTION()
+    void OnAcceptClicked();
+
+    UFUNCTION()
+    void OnDeclineClicked();
+
+    UFUNCTION()
+    void OnMouseChange();
+	
+protected:
+    UPROPERTY( EditAnywhere , BlueprintReadOnly , meta = (BindWidget) )
+    class UTextBlock* txt_QuestName;
+
+    UPROPERTY( EditAnywhere , BlueprintReadOnly , meta = (BindWidget) )
+    class UTextBlock* txt_QuestDesc;
+
+    UPROPERTY( EditAnywhere , BlueprintReadOnly , meta = (BindWidget) )
+    class UTextBlock* txt_StageDesc;
+
+    UPROPERTY( EditAnywhere , BlueprintReadOnly , meta = (BindWidget) )
+    class UButton* btn_Decline;
+
+    UPROPERTY( EditAnywhere , BlueprintReadOnly , meta = (BindWidget) )
+    class UButton* btn_Accept;
+
+};
