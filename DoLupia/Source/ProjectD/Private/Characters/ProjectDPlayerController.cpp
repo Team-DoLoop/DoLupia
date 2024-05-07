f1809baec5f627ea3e1790643dea6d290f44ff74
Seokhee Kim <69100145+yehang218@users.noreply.github.com>
2024-05-07 18:03:53 +0900

    👀 Test : PlayerController -> Attack() -> TakeDamage

--- DoLupia/Source/ProjectD/Private/Characters/ProjectDPlayerController.cpp
+++ DoLupia/Source/ProjectD/Private/Characters/ProjectDPlayerController.cpp
@@ -191,4 +191,7 @@ void AProjectDPlayerController::Attack()
 	if(!ControlledCharacter) return;
 	
 	ControlledCharacter->attackComp->Attack();
+
+	// Test
+	ControlledCharacter->TakeDamage(10.0f);
 }
