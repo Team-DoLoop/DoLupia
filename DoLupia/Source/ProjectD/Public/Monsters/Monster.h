492b583468eb426f6523a431fed19cc948fc2184
Minseo Kim <minseo9865@naver.com>
2024-05-10 17:35:26 +0900

    Update RangedMonster 공격패턴

--- DoLupia/Source/ProjectD/Public/Monsters/Monster.h
+++ DoLupia/Source/ProjectD/Public/Monsters/Monster.h
@@ -78,7 +78,7 @@ public:
 
 	//일정 반경 안에 들어오면 플레이어를 향해 이동
 	UPROPERTY( EditAnywhere )
-	float TargetRange = 900;
+	float TargetRange = 1500;
 
 	//일정 반경 안에 들어오면 공격모드로 전환
 	UPROPERTY( EditAnywhere )
--- DoLupia/Source/ProjectD/Public/Monsters/RangedMonster.h
+++ DoLupia/Source/ProjectD/Public/Monsters/RangedMonster.h
@@ -31,5 +31,19 @@ public:
 	//virtual void MoveState() override;
 	virtual void AttackState() override;
 
-	float attackDelayTime = 3;
+	float attackDelayTime = 6;
+
+	UPROPERTY( EditAnywhere )
+	float DelayTime = 3;
+
+	//Looping 여부
+	UPROPERTY( EditAnywhere )
+	bool bIsLoop = true;
+
+	//마법 발사
+	void MagicAttack();
+
+	float currentTimeRM = 0;
+
+	bool isPaused = false;
 };
