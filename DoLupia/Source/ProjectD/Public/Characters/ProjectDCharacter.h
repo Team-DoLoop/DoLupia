149847190d41e5a9a6e3557e02b8c010da855711
YunTaeHo <taehoyun0407@gmail.com>
2024-05-21 09:26:09 +0900

    플레이어 렌더 타겟 UI 및 장비 장착 슬롯 활성화

--- DoLupia/Source/ProjectD/Public/Characters/ProjectDCharacter.h
+++ DoLupia/Source/ProjectD/Public/Characters/ProjectDCharacter.h
@@ -119,6 +119,8 @@ public:
 	UPlayerDefaultsWidget* GetPlayerDefaultsWidget() const { return PlayerDefaultsWidget;}
 	void UseQuickSlot(int32 SlotNumber);
 
+	ADoLupiaHUD* GetDoLupiaHUD() { return HUD; }
+
 	
 
 	// <---------------------- Move ---------------------->
