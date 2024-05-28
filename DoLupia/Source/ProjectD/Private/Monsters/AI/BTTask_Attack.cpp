804e5d2429f32fbdbb6b54fb23df1cb9a3617205
Minseo Kim <minseo9865@naver.com>
2024-05-28 23:29:39 +0900

    Update Boss Monster

---
+++ DoLupia/Source/ProjectD/Private/Monsters/AI/BTTask_Attack.cpp
@@ -0,0 +1,58 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Monsters/AI/BTTask_Attack.h"
+#include "Monsters/MonsterAIController.h"
+#include "Monsters/BossMonster.h"
+UBTTask_Attack::UBTTask_Attack()
+{
+	bNotifyTick = true; // 매 프레임마다 TickTask 호출
+	IsAttacking = false; //현재 공격 중인지
+}
+
+EBTNodeResult::Type UBTTask_Attack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	EBTNodeResult::Type Result = Super::ExecuteTask( OwnerComp , NodeMemory );
+
+	auto Boss = Cast<ABossMonster>( OwnerComp.GetAIOwner()->GetPawn() );
+	if (nullptr == Boss)
+		return EBTNodeResult::Failed;
+
+	UE_LOG( LogTemp , Warning , TEXT( "UBTTask_Attack::ExecuteTask" ) );
+
+	//Boss->OnAttackEnd.AddDynamic( this , &UBTTask_Attack::OnAttackEndHandle );
+	Boss->Attack();
+	IsAttacking = true;
+
+	//ct += GetWorld()->GetDeltaSeconds();
+	//if(ct>3)
+	//{
+	//	//IsAttacking = false;
+	//	ct = 0;
+	//	return EBTNodeResult::Succeeded;
+
+	//}
+
+	/*if (!IsAttacking)
+	{
+		UE_LOG( LogTemp , Warning , TEXT( "UBTTask_Attack::공격 끝" ) );
+
+		return EBTNodeResult::Succeeded;
+	}*/
+	return EBTNodeResult::Succeeded;
+
+	//return EBTNodeResult::InProgress;
+}
+
+void UBTTask_Attack::TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickTask(OwnerComp , NodeMemory , DeltaSeconds);
+
+	
+}
+
+void UBTTask_Attack::OnAttackEndHandle()
+{
+	IsAttacking = false;
+}
+
--- DoLupia/Source/ProjectD/Private/Monsters/BossMonster.cpp
+++ DoLupia/Source/ProjectD/Private/Monsters/BossMonster.cpp
@@ -8,12 +8,32 @@
 #include "Engine/EngineTypes.h"  // 필요
 #include "Engine/Engine.h"  // GEngine
 #include "Engine/OverlapResult.h"
+#include "Monsters/AI/BTTask_Attack.h"
 
 // Sets default values
 ABossMonster::ABossMonster()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+
+	ConstructorHelpers::FObjectFinder<USkeletalMesh>BossMonsterMesh( TEXT( "/Game/Asset/Character/Sci_Fi_Droid_Mee/Mesh/Character/SK_Mee_Droid_Mesh.SK_Mee_Droid_Mesh" ) );
+	if (BossMonsterMesh.Succeeded()) {
+		GetMesh()->SetSkeletalMesh( BossMonsterMesh.Object );
+	}
+	/*
+	OctopusBackpackComponent = CreateDefaultSubobject<UOctopusBackpackComponent>( TEXT( "OctopusBackpackComponent" ) );
+	ChildActorComponent = CreateDefaultSubobject<UChildActorComponent>( TEXT( "ChildActorComponent" ) );
+
+	FName OctoSocket( TEXT( "OctoSocket" ) );
+	if (GetMesh()->DoesSocketExist( OctoSocket ))
+	{
+		ChildActorComponent->SetupAttachment( GetMesh(),OctoSocket );
+
+	}
+	*/
+	//ChildActorComponent->SetupAttachment( GetMesh());
+
+
 
 	AIControllerClass = AMonsterAIController::StaticClass();
 	AutoPossessAI = EAutoPossessAI::PlacedInWorldOrSpawned;
@@ -25,13 +45,24 @@ ABossMonster::ABossMonster()
 void ABossMonster::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	//auto BTTask_Attack= Cast<UBTTask_Attack>()
+	//BTTask_Attack->OnAttackEndHandle.AddDynamic( this , &ABossMonster::Attack );
+	//if (GetMesh())ChildActorComponent->SetupAttachment( GetMesh() );
 }
 
 // Called every frame
-void ABossMonster::Tick(float DeltaTime)
+void ABossMonster::Tick( float DeltaTime )
 {
-	Super::Tick(DeltaTime);
+	Super::Tick( DeltaTime );
 
+}
+
+void ABossMonster::Attack()
+{
+	UE_LOG( LogTemp , Warning , TEXT( "ABossMonster::Attack() - 공격 시작" ) );
+	//OnAttackEnd.Broadcast();
+
+	
 }
 
